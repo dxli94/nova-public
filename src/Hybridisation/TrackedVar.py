1c28ec4f46bfec3fbed74d0c4cec8698e4b189a4
Dongxu  Li <u5886609@anu.edu.au>
2018-09-03 11:45:31 +1000

    Create TrackedVar

---
+++ src/Hybridisation/TrackedVar.py
@@ -0,0 +1,37 @@
+class TrackedVar:
+    def __init__(self, val):
+        self.prev = None
+        self.curr = val
+
+    def set_val(self, val):
+        self.prev = self.curr
+        self.curr = val
+
+    def get_val(self):
+        assert self.curr, "No current value found."
+
+        return self.curr
+
+    def rollback(self):
+        assert self.prev, "No previous value found."
+
+        self.curr = self.prev
+        self.prev = None
+
+if __name__ == '__main__':
+    # test TrackedVar()
+    tv = TrackedVar(1)
+    assert tv.get_val() == 1
+    tv.set_val(2)
+    assert tv.get_val() == 2
+    tv.rollback()
+    assert tv.get_val() == 1
+    tv.set_val(3)
+    assert tv.get_val() == 3
+    tv.rollback()
+    assert tv.get_val() == 1
+
+
+
+
+
