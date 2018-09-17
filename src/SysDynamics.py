3f383a8cdd688590d1f9dd0f0758573933932a56
Dongxu  Li <u5886609@anu.edu.au>
2018-09-17 16:31:38 +1000

    minor.

--- src/SysDynamics.py
+++ src/SysDynamics.py
@@ -50,9 +50,6 @@ class GeneralDynamics:
         self.str_rep = args
 
     def eval(self, vals):
-        assert len(vals) == len(self.state_vars), \
-            "inconsistent number of variables, {} is expected, {} is given".format(len(self.state_vars), len(vals))
-
         return self.lamdafied_dynamics(*vals)
 
     def eval_jacobian(self, vals):
@@ -69,14 +66,48 @@ class GeneralDynamics:
 
 
 if __name__ == '__main__':
-    id_to_vars = {0: 'x0',
-                  1: 'x1',
-                  2: 'x2'}
-    gd = GeneralDynamics(id_to_vars, 'x0/x1', 'x1+x2')
+    # id_to_vars = {0: 'x0',
+    #               1: 'x1',
+    #               2: 'x2'}
+    # gd = GeneralDynamics(id_to_vars, 'x0/x1', 'x1+x2')
+    #
+    # for i in range(0, 500000):
+    #     if i % 10000 == 0:
+    #         print(i)
+    #     gd.eval_jacobian([1, 2, 3])
+    #
+    # pass
 
-    for i in range(0, 500000):
-        if i % 10000 == 0:
-            print(i)
-        gd.eval_jacobian([1, 2, 3])
+    from multiprocessing import Pool
+    import dill
 
-    pass
+    dill.settings['recurse'] = True
+    # from pathos.multiprocessing import Pool
+
+    x = sympy.symbols('x')
+    expr = sympy.sympify('x*x')
+
+    import time
+
+    def basinhopping_wrapper(expr, x, vals):
+        jacobian_lambda = sympy.lambdify(x, sympy.Matrix([expr]).jacobian([x]))
+        return jacobian_lambda(*vals)
+
+
+    # for i in range(10000):
+    jacobian_lambda = sympy.lambdify(x, sympy.Matrix([expr]).jacobian([x]))
+
+    pool = Pool()
+
+    start = time.time()
+    # for i in range(1000):
+    #     res1 = pool.apply_async(basinhopping_wrapper, [expr, x, [1]])
+    #     res2 = pool.apply_async(basinhopping_wrapper, [expr, x, [2]])
+    #
+    #     a = [res1.get(), res2.get()]
+
+    for i in range(1000):
+        basinhopping_wrapper(expr, x, [1])
+        basinhopping_wrapper(expr, x, [2])
+
+    print(time.time()-start)
